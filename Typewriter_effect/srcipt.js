84444f7a595f9106116f9760d7257096e1845dde
Lokesh <lokeshsingh7695@gmail.com>
2023-08-02 23:42:13 +0530

    day3 Project

---
+++ Typewriter_effect/srcipt.js
@@ -0,0 +1,20 @@
+var get = document.getElementById("get");
+
+var txt = get.innerText;
+
+let s = "";
+
+i = 0;
+const typewriter = () => {
+  s += txt.charAt(i) + "|";
+  get.innerText = s;
+  s = s.slice(0, -1);
+  i++;
+  if (i >= txt.length) {
+    i = 0;
+    s = "";
+  }
+  setTimeout(typewriter, 200);
+};
+
+typewriter();
