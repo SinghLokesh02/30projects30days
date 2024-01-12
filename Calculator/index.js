e60c422faebc7a872665718f948205548714439c
Lokesh <lokeshsingh7695@gmail.com>
2024-01-12 17:26:24 +0530

    Calculator using HTML,CSS and Js

---
+++ Calculator/index.js
@@ -0,0 +1,33 @@
+let curDisplay =  '';
+document.querySelector('#display').value = curDisplay;
+
+let buttons = document.querySelectorAll('input[type="button"]');
+console.log(buttons);
+for(button of buttons) {
+    button.addEventListener('click', function(event) {
+        let value = event.target.value;
+        console.log(value);
+        if(value === '='){
+            curDisplay = eval(curDisplay);
+        }
+        else if(value === 'AC'){
+            curDisplay = '0';
+        }
+        else if(value === '<-'){
+            curDisplay = curDisplay.slice(0,-1);
+        }
+        else{
+            if(curDisplay === '0')
+            curDisplay = value
+            else{
+                curDisplay += value;
+            }
+        }
+        
+    document.querySelector('#display').value = curDisplay;
+});
+}
+
+
+ 
+ 
